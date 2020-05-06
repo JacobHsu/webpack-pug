48d921394730ca7e31b997ad466e55dd5b54af06
jacobhsu <jacob.hsu.tw@gmail.com>
2020-05-06 17:41:43 +0800

    use webpack4 compile .pug(jade) to html

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # webpack-pug
+
+`$ yarn init`  
+`$ yarn add -D webpack webpack-cli webpack-dev-server`
+
+由於需要輸出HTML，所以需要安裝html-webpack-plugin
+
+`$ yarn add -D html-webpack-plugin`  
+
+`$ yarn add -D pug html-loader pug-html-loader`  
+
+`$ yarn add -D gh-pages`
+
+## References
+
+[如果你是常切版的前端工程師，你一定要知道pug!](https://medium.com/@NorthBei/如果你是常切版的前端工程師-你一定要知道pug-8b2cbc0a784c)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {   
+        rules: [{
+                test: /\.pug$/,
+                use: ['html-loader','pug-html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.pug',
+            // 輸出的文件名稱，要注意的是這邊的root path是module.exports.output.path (在這個project內就是build folder)
+            filename:'./index.html'
+        })
+    ],
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 500,
+        poll: 1000,
+        ignored: /node_modules/
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'build'),
+        compress: true,
+        port: 8000
+    }
+}
